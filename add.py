c7e8e1026754be197e49c4dddbe7569ecbb3474f
Dharshan <dharshan280706@gmail.com>
2025-12-19 10:19:17 +0530

    Removed sub

--- add.py
+++ add.py
@@ -2,9 +2,7 @@ a = int(input("Enter n1: "))
 b = int(input("Enter n2: "))
 c = a + b
 d = a * b
-e = a - b
 print("Sum = ",c)
 print("Product: ",d)
-print("Subtraction: ",e)
 f = a / b
 print("Division: ",f)
