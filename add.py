bb67b32a755e560ec91e368ac61d1dc8cc0e80b6
Dharshan <dharshan280706@gmail.com>
2025-12-26 09:12:17 +0530

    sub add panniruken

--- add.py
+++ add.py
@@ -2,7 +2,9 @@ a = int(input("Enter n1: "))
 b = int(input("Enter n2: "))
 c = a + b
 d = a * b
+e = a - b
 print("Sum = ",c)
 print("Product: ",d)
 f = a / b
 print("Division: ",f)
+print("Difference: ",e)
