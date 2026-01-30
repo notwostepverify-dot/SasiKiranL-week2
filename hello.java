30266dc26102def05048eb961471a59f0266bfda
notwostepverify-dot <notwostepverify@gmail.com>
2026-01-30 09:54:22 +0530

    Remove floating point and unary operations
    Removed floating point operations and unary operations from the output.

--- hello.java
+++ hello.java
@@ -11,29 +11,12 @@ public class hello {
         int quot = a / b;
         int rem = a % b;
 
-        double dSum = x + y;
-        double dDiff = x - y;
-        double dProd = x * y;
-        double dQuot = x / y;
-
         System.out.println("Integer Ops:");
         System.out.println("Sum: " + sum);
         System.out.println("Difference: " + diff);
         System.out.println("Product: " + prod);
         System.out.println("Quotient: " + quot);
         System.out.println("Remainder: " + rem);
-
-        System.out.println("\nFloating Point Ops:");
-        System.out.println("Sum: " + dSum);
-        System.out.println("Difference: " + dDiff);
-        System.out.println("Product: " + dProd);
-        System.out.println("Quotient: " + dQuot);
-        
-        a++;
-        b--;
-        System.out.println("\nUnary Ops:");
-        System.out.println("Incremented a: " + a);
-        System.out.println("Decremented b: " + b);
     }
 }
 
